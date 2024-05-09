65eaa6699337231176d5b4943e1a5a23e11afb81
Vishvananda Abrams <vishvananda@gmail.com>
2024-05-08 18:38:11 -0700

    fix redis url

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 export OAUTH2_PROXY_OIDC_ISSUER_URL="${OAUTH2_PROXY_OIDC_ISSUER_URL:=https://hk-id.my.salesforce.com}"
 export REDIS_URL="${CACHE_URL:-$REDIS_URL}"
+export REDIS_URL="${CACHE_TLS_URL:-$REDIS_URL}"
 if [[ "$REDIS_URL" != "" ]]; then
-    EXTRA="--session-store-type=redis --redis-connection-url $CACHE_URL --redis-insecure-skip-tls-verify"
+    EXTRA="--session-store-type=redis --redis-connection-url $REDIS_URL --redis-insecure-skip-tls-verify"
 fi
 exec ./bin/oauth2-proxy --email-domain '*' --alpha-config config.yaml --standard-logging --auth-logging --request-logging --skip-provider-button=true $EXTRA
